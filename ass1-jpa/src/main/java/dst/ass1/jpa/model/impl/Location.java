df440308cba331ee07fb7530dbdc9f076c214759
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-08 15:15:45 +0100

    added trip entity skeleton

---
+++ ass1-jpa/src/main/java/dst/ass1/jpa/model/impl/Location.java
@@ -0,0 +1,48 @@
+package dst.ass1.jpa.model.impl;
+
+import dst.ass1.jpa.model.ILocation;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Location implements ILocation {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Long locationId;
+
+
+    @Override
+    public Long getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(Long id) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public Long getLocationId() {
+        return null;
+    }
+
+    @Override
+    public void setLocationId(Long locationId) {
+
+    }
+}
