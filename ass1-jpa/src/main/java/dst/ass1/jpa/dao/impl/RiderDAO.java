ff429a13cfc53b0964178b50e454127ba3729228
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-16 22:41:55 +0100

    added jpa query skeleton

--- ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/RiderDAO.java
+++ ass1-jpa/src/main/java/dst/ass1/jpa/dao/impl/RiderDAO.java
@@ -2,11 +2,15 @@ package dst.ass1.jpa.dao.impl;
 
 import dst.ass1.jpa.dao.IRiderDAO;
 import dst.ass1.jpa.model.IRider;
+import dst.ass1.jpa.model.TripState;
 import dst.ass1.jpa.model.impl.Rider;
+import dst.ass1.jpa.model.impl.Trip;
 import dst.ass1.jpa.util.Constants;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
@@ -37,7 +41,36 @@ public class RiderDAO extends BasicDAOImpl<IRider> implements IRiderDAO {
 
     @Override
     public List<IRider> findTopThreeRidersWithMostCanceledTripsAndRatingLowerEqualTwo(Date start, Date end) {
-        return null;
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
+        CriteriaQuery<Rider> cq = cb.createQuery(Rider.class);
+        Root<Rider> rider = cq.from(Rider.class);
+
+/*
+        Join<Rider, Trip> trip = rider.join("trips",JoinType.INNER);
+
+        Predicate predicate = null;
+
+        if (start == null && end == null) {
+            System.err.println("Should have right ");
+        } else if (start == null) {
+            predicate = cb.lessThanOrEqualTo(trip.get("created"), end);
+        } else if (end == null) {
+            predicate = cb.greaterThanOrEqualTo(trip.get("created"), start);
+
+        } else {
+            predicate = cb.between(trip.get("created"), start, end);
+        }
+
+        cb.and(predicate);
+*/
+
+        cq.select(rider).where(cb.le(rider.get("avgRating"), 2.0));
+
+        TypedQuery<Rider> q = em.createQuery(cq);
+
+
+        return new ArrayList<>(q.setMaxResults(3).getResultList());
     }
 
     @Override
