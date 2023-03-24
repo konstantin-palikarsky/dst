2e3bb7811e65bd7e99ae4ede6e101be23c367da0
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-24 18:33:49 +0100

    added transactions to session creation

--- ass1-kv/src/main/java/dst/ass1/kv/impl/SessionManager.java
+++ ass1-kv/src/main/java/dst/ass1/kv/impl/SessionManager.java
@@ -4,6 +4,7 @@ import dst.ass1.kv.ISessionManager;
 import dst.ass1.kv.SessionCreationFailedException;
 import dst.ass1.kv.SessionNotFoundException;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -12,8 +13,8 @@ public class SessionManager implements ISessionManager {
     private static final String USERID_KEY = "userId";
     JedisPool redisPool;
 
-    public SessionManager(String host, int port){
-        redisPool = new JedisPool(host,port);
+    public SessionManager(String host, int port) {
+        redisPool = new JedisPool(host, port);
     }
 
     /**
@@ -24,54 +25,63 @@ public class SessionManager implements ISessionManager {
     public String createSession(Long userId, int timeToLive) throws SessionCreationFailedException {
 
         var sessionToken = UUID.randomUUID().toString();
-        var hashVariables = new HashMap<String,String>();
-        hashVariables.put(USERID_KEY,userId.toString());
+        var hashVariables = new HashMap<String, String>();
+        hashVariables.put(USERID_KEY, userId.toString());
 
-        redisPool.getResource().hset(sessionToken,hashVariables);
-        redisPool.getResource().expire(sessionToken,timeToLive);
+        var redisClient = redisPool.getResource();
+        redisClient.watch(sessionToken);
+
+        try (Transaction t = redisClient.multi()) {
+            redisClient.hset(sessionToken, hashVariables);
+            redisClient.expire(sessionToken, timeToLive);
+            if (t.exec() == null) {
+                throw new SessionCreationFailedException();
+            }
+        }
 
         return sessionToken;
     }
 
     @Override
     public void setSessionVariable(String sessionId, String key, String value) throws SessionNotFoundException {
-        var hashVariables = new HashMap<String,String>();
-        hashVariables.put(key,value);
+        var hashVariables = new HashMap<String, String>();
+        hashVariables.put(key, value);
 
-        var session = redisPool.getResource().hgetAll(sessionId);
-        if (session.keySet().isEmpty()){
+        var redisClient = redisPool.getResource();
+
+        var session = redisClient.hgetAll(sessionId);
+        if (session.keySet().isEmpty()) {
             throw new SessionNotFoundException();
         }
 
-        redisPool.getResource().hset(sessionId,hashVariables);
+        redisClient.hset(sessionId, hashVariables);
     }
 
     @Override
     public String getSessionVariable(String sessionId, String key) throws SessionNotFoundException {
-
         var session = redisPool.getResource().hgetAll(sessionId);
-        if (session.keySet().isEmpty()){
+
+        if (session.keySet().isEmpty()) {
             throw new SessionNotFoundException();
-        }else{
+        } else {
             return session.get(key);
         }
-
     }
 
     @Override
     public Long getUserId(String sessionId) throws SessionNotFoundException {
-        return Long.parseLong(getSessionVariable(sessionId,USERID_KEY));
+        return Long.parseLong(getSessionVariable(sessionId, USERID_KEY));
     }
 
     @Override
     public int getTimeToLive(String sessionId) throws SessionNotFoundException {
         var ttl = redisPool.getResource().ttl(sessionId);
 
-        if (ttl==-2){
+        if (ttl == -2) {
             throw new SessionNotFoundException();
         }
 
-        return (int)ttl;
+        return (int) ttl;
     }
 
     @Override
