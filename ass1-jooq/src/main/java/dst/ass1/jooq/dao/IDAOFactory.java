171adbd3c52baa7203655112e92e1cc1bc0ceecd
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-24 15:22:31 +0100

    added template patch

--- ass1-jooq/src/main/java/dst/ass1/jooq/dao/IDAOFactory.java
+++
@@ -1,7 +0,0 @@
-package dst.ass1.jooq.dao;
-
-public interface IDAOFactory {
-
-
-  IRiderPreferenceDAO createRiderPreferenceDao();
-}
--- ass1-jooq/src/main/java/dst/ass1/jooq/model/IModelFactory.java
+++
@@ -1,6 +0,0 @@
-package dst.ass1.jooq.model;
-
-public interface IModelFactory {
-
-  IRiderPreference createRiderPreference();
-}
