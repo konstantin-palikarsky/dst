a760ca3ee6cfcf1e31e9cfdab338dbf39414766a
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-24 20:34:38 +0100

    import pruning

--- ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentRepository.java
+++ ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentRepository.java
@@ -2,9 +2,7 @@ package dst.ass1.doc.impl;
 
  import dst.ass1.doc.IDocumentRepository;
  import dst.ass1.jpa.model.ILocation;
- import org.bson.Document;
 
-import java.util.List;
  import java.util.Map;
 
 public class DocumentRepository implements IDocumentRepository {
