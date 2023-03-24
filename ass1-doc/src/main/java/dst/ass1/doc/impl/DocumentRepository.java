e7145264f61300f26e1b4b6d0437e1510447280e
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-24 21:16:33 +0100

    connected to the mongodb by name in doc repo

--- ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentRepository.java
+++ ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentRepository.java
@@ -1,11 +1,25 @@
 package dst.ass1.doc.impl;
 
+ import com.mongodb.MongoClient;
+ import com.mongodb.client.MongoDatabase;
  import dst.ass1.doc.IDocumentRepository;
  import dst.ass1.jpa.model.ILocation;
+ import dst.ass1.jpa.util.Constants;
 
  import java.util.Map;
 
 public class DocumentRepository implements IDocumentRepository {
+
+    private final MongoDatabase db;
+
+    public DocumentRepository(){
+       try( MongoClient mongoClient = new MongoClient()){
+
+           db = mongoClient.getDatabase(Constants.MONGO_DB_NAME);
+
+       }
+    }
+
     @Override
     public void insert(ILocation location, Map<String, Object> locationProperties) {
 
