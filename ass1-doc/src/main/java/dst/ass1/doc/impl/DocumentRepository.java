752d299e191217acfa8ec77aad30f177eda9023e
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-25 12:14:03 +0100

    added mongodb inserting

--- ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentRepository.java
+++ ass1-doc/src/main/java/dst/ass1/doc/impl/DocumentRepository.java
@@ -1,27 +1,39 @@
 package dst.ass1.doc.impl;
 
- import com.mongodb.MongoClient;
- import com.mongodb.client.MongoDatabase;
- import dst.ass1.doc.IDocumentRepository;
- import dst.ass1.jpa.model.ILocation;
- import dst.ass1.jpa.util.Constants;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import dst.ass1.doc.IDocumentRepository;
+import dst.ass1.jpa.model.ILocation;
+import dst.ass1.jpa.util.Constants;
+import org.bson.Document;
 
- import java.util.Map;
+import java.util.Map;
 
-public class DocumentRepository implements IDocumentRepository {
+public class DocumentRepository implements IDocumentRepository, AutoCloseable {
 
+    private final MongoClient mongoClient ;
     private final MongoDatabase db;
 
     public DocumentRepository(){
-       try( MongoClient mongoClient = new MongoClient()){
-
-           db = mongoClient.getDatabase(Constants.MONGO_DB_NAME);
-
-       }
+        mongoClient = new MongoClient();
+        db = mongoClient.getDatabase(Constants.MONGO_DB_NAME);
     }
 
     @Override
     public void insert(ILocation location, Map<String, Object> locationProperties) {
+        MongoCollection<Document> collection = db.getCollection(Constants.COLL_LOCATION_DATA);
 
+        Document doc = new Document();
+        locationProperties.put("name", location.getName());
+        locationProperties.put("location_id", location.getLocationId());
+        doc.putAll(locationProperties);
+
+        collection.insertOne(doc);
+    }
+
+    @Override
+    public void close() throws Exception {
+        mongoClient.close();
     }
 }
