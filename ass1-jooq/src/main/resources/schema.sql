3650ca0cb7db710b3449d8a9a5e2a1fb348a176c
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-03-26 21:00:00 +0200

    created jooq database schema

--- ass1-jooq/src/main/resources/schema.sql
+++ ass1-jooq/src/main/resources/schema.sql
@@ -9,3 +9,21 @@
 DROP TABLE IF EXISTS preference, rider_preference;
 
 -- Add here your sql statements to create the tables "preference" and "rider_preference"
+CREATE TABLE rider_preference
+(
+    rider_id      BIGINT NOT NULL,
+    vehicle_class VARCHAR(255),
+    area          VARCHAR(255),
+    primary key (rider_id)
+);
+
+CREATE TABLE preference
+(
+    id         BIGINT NOT NULL,
+    rider_id   BIGINT,
+    pref_key   VARCHAR(255),
+    pref_value VARCHAR(255),
+    primary key (id),
+    foreign key (rider_id) references rider_preference (rider_id)
+);
+
