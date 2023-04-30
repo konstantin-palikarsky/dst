0e5c7de1b8fa5f9e765afeffa4850690de3a09e1
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-04-30 23:19:49 +0200

    implemented rest client service facade

--- ass2-service/facade/src/main/java/dst/ass2/service/facade/impl/TripServiceFacade.java
+++ ass2-service/facade/src/main/java/dst/ass2/service/facade/impl/TripServiceFacade.java
@@ -3,42 +3,75 @@ package dst.ass2.service.facade.impl;
 import dst.ass2.service.api.trip.*;
 import dst.ass2.service.api.trip.rest.ITripServiceResource;
 import dst.ass2.service.facade.impl.providers.filter.RequireAuth;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.HttpUrlConnectorProvider;
+import org.glassfish.jersey.client.proxy.WebResourceFactory;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URL;
 
 @Path("/trips")
 public class TripServiceFacade implements ITripServiceResource {
+    @Inject
+    private URL tripServiceURI;
+    private ITripServiceResource realService;
+
+    @PostConstruct
+    private void setup() {
+        var configuration = new ClientConfig()
+                .property(HttpUrlConnectorProvider.SET_METHOD_WORKAROUND, true)
+                .property(ClientProperties.SUPPRESS_HTTP_COMPLIANCE_VALIDATION, true);
+
+
+        Client client = ClientBuilder.newClient(configuration);
+        WebTarget webTarget = client.target(tripServiceURI.toString());
+
+        realService = WebResourceFactory.newResource(ITripServiceResource.class, webTarget);
+    }
 
     @Override
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
-    public Response getTrip(@PathParam("id") Long tripId) throws EntityNotFoundException {
-        throw new RuntimeException("Unimplemented");
+    public Response getTrip(@PathParam("id") Long tripId)
+            throws EntityNotFoundException {
 
+        return realService.getTrip(tripId);
     }
 
     @Override
     @POST
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
     public Response createTrip(@FormParam("riderId") Long riderId,
                                @FormParam("pickupId") Long pickupId,
                                @FormParam("destinationId") Long destinationId)
             throws EntityNotFoundException {
-        throw new RuntimeException("Unimplemented");
 
+        return realService.createTrip(riderId, pickupId, destinationId);
     }
 
     @Override
     @DELETE
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
-    public Response deleteTrip(@PathParam("id") Long tripId) throws EntityNotFoundException {
-        throw new RuntimeException("Unimplemented");
+    public Response deleteTrip(@PathParam("id") Long tripId)
+            throws EntityNotFoundException {
 
+        return realService.deleteTrip(tripId);
     }
 
 
@@ -46,64 +79,76 @@ public class TripServiceFacade implements ITripServiceResource {
     @POST
     @Path("/{id}/stops")
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
     public Response addStop(@PathParam("id") Long tripId,
                             @FormParam("locationId") Long locationId)
             throws EntityNotFoundException {
-        throw new RuntimeException("Unimplemented");
 
-
+        return realService.addStop(tripId, locationId);
     }
 
     @Override
     @DELETE
     @Path("/{id}/stops/{locationId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
     public Response removeStop(@PathParam("id") Long tripId,
                                @PathParam("locationId") Long locationId)
             throws EntityNotFoundException {
 
-        throw new RuntimeException("Unimplemented");
-
+        return realService.removeStop(tripId, locationId);
     }
 
     @Override
     @PATCH
     @Path("/{id}/confirm")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
-    public Response confirm(@PathParam("id") Long tripId) throws EntityNotFoundException, InvalidTripException {
-        throw new RuntimeException("Unimplemented");
+    public Response confirm(@PathParam("id") Long tripId)
+            throws EntityNotFoundException, InvalidTripException {
 
+        return realService.confirm(tripId);
     }
 
 
     @Override
     @POST
     @Path("/{id}/match")
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
     public Response match(@PathParam("id") Long tripId,
                           MatchDTO matchDTO)
             throws EntityNotFoundException, DriverNotAvailableException {
-        throw new RuntimeException("Unimplemented");
 
+        return realService.match(tripId, matchDTO);
     }
 
     @Override
     @PATCH
     @Path("/{id}/cancel")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @RequireAuth
-    public Response cancel(@PathParam("id") Long tripId) throws EntityNotFoundException {
-        throw new RuntimeException("Unimplemented");
+    public Response cancel(@PathParam("id") Long tripId)
+            throws EntityNotFoundException {
 
+        return realService.cancel(tripId);
     }
 
     @Override
     @POST
     @Path("/{id}/complete")
-    @RequireAuth
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response complete(@PathParam("id") Long tripId, TripInfoDTO tripInfoDTO) throws EntityNotFoundException {
-        throw new RuntimeException("Unimplemented");
+    @RequireAuth
+    public Response complete(@PathParam("id") Long tripId, TripInfoDTO tripInfoDTO)
+            throws EntityNotFoundException {
+
+        return realService.complete(tripId, tripInfoDTO);
     }
+
 }
