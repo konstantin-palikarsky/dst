8479b709fb99ffd4a63d05fba0862f5118e0b0fc
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-04-30 09:20:20 +0200

    created add-stops method

--- ass2-service/trip/src/main/java/dst/ass2/service/trip/impl/TripService.java
+++ ass2-service/trip/src/main/java/dst/ass2/service/trip/impl/TripService.java
@@ -4,9 +4,10 @@ import dst.ass1.jpa.dao.IDAOFactory;
 import dst.ass1.jpa.dao.ILocationDAO;
 import dst.ass1.jpa.dao.IRiderDAO;
 import dst.ass1.jpa.dao.ITripDAO;
+import dst.ass1.jpa.model.ILocation;
+import dst.ass1.jpa.model.IModelFactory;
 import dst.ass1.jpa.model.ITrip;
 import dst.ass1.jpa.model.TripState;
-import dst.ass1.jpa.model.impl.Trip;
 import dst.ass2.service.api.match.IMatchingService;
 import dst.ass2.service.api.trip.*;
 
@@ -16,6 +17,7 @@ import javax.ejb.Singleton;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
+import java.util.stream.Collectors;
 
 @Singleton
 @Named
@@ -28,6 +30,9 @@ public class TripService implements ITripService {
 
     @Inject
     private IMatchingService matchingService;
+
+    @Inject
+    private IModelFactory modelFactory;
 
     private ITripDAO tripRepository;
     private IRiderDAO riderRepository;
@@ -54,7 +59,7 @@ public class TripService implements ITripService {
             throw new EntityNotFoundException("No such " + missingAttribute);
         }
 
-        ITrip newTrip = new Trip();
+        ITrip newTrip = modelFactory.createTrip();
         newTrip.setRider(rider);
         newTrip.setPickup(pickup);
         newTrip.setDestination(destination);
@@ -62,13 +67,7 @@ public class TripService implements ITripService {
 
         ITrip persistedTrip = tripRepository.save(newTrip);
         var dto = mapTripToDto(persistedTrip);
-        MoneyDTO fare;
-
-        try {
-            fare = matchingService.calculateFare(dto);
-        } catch (InvalidTripException e) {
-            fare = null;
-        }
+        MoneyDTO fare = safelyCalculateFare(dto);
         dto.setFare(fare);
 
         return dto;
@@ -76,42 +75,72 @@ public class TripService implements ITripService {
 
     @Override
     public void confirm(Long tripId) throws EntityNotFoundException, IllegalStateException, InvalidTripException {
-
+        throw new RuntimeException();
     }
 
     @Override
     public void match(Long tripId, MatchDTO match) throws EntityNotFoundException, DriverNotAvailableException, IllegalStateException {
-
+        throw new RuntimeException();
     }
 
     @Override
     public void complete(Long tripId, TripInfoDTO tripInfoDTO) throws EntityNotFoundException {
-
+        throw new RuntimeException();
     }
 
     @Override
     public void cancel(Long tripId) throws EntityNotFoundException {
-
+        throw new RuntimeException();
     }
 
     @Override
     public boolean addStop(TripDTO trip, Long locationId) throws EntityNotFoundException, IllegalStateException {
-        return false;
+
+        if (trip.getStops().contains(locationId)) {
+            return false;
+        }
+
+        var tripEntity = tripRepository.findById(trip.getId());
+        if (tripEntity == null) {
+            throw new RuntimeException("DTO of non-existent entity at runtime");
+        }
+
+        if (!tripEntity.getState().equals(TripState.CREATED)) {
+            throw new IllegalStateException("This trip's route can no longer be modified");
+        }
+
+        var locationEntity = locationRepository.findById(locationId);
+        if (locationEntity == null) {
+            throw new EntityNotFoundException("Cannot add a stop at an non-existent location");
+        }
+
+        tripEntity.addStop(locationEntity);
+        tripRepository.save(tripEntity);
+
+        var newTripStops = trip.getStops();
+        newTripStops.add(locationId);
+        trip.setStops(newTripStops);
+
+        var fare = safelyCalculateFare(trip);
+        trip.setFare(fare);
+
+        return true;
     }
 
     @Override
     public boolean removeStop(TripDTO trip, Long locationId) throws EntityNotFoundException, IllegalStateException {
-        return false;
+        throw new RuntimeException();
     }
 
     @Override
     public void delete(Long tripId) throws EntityNotFoundException {
+        throw new RuntimeException();
 
     }
 
     @Override
     public TripDTO find(Long tripId) {
-        return null;
+        throw new RuntimeException();
     }
 
     private TripDTO mapTripToDto(ITrip trip) {
@@ -121,6 +150,20 @@ public class TripService implements ITripService {
         dto.setRiderId(trip.getRider().getId());
         dto.setId(trip.getId());
 
+        if (trip.getStops() != null) {
+            dto.setStops(trip.getStops().stream().map(ILocation::getId).collect(Collectors.toList()));
+        }
+
         return dto;
+    }
+
+    private MoneyDTO safelyCalculateFare(TripDTO trip) {
+        MoneyDTO fare;
+        try {
+            fare = matchingService.calculateFare(trip);
+        } catch (InvalidTripException e) {
+            fare = null;
+        }
+        return fare;
     }
 }
