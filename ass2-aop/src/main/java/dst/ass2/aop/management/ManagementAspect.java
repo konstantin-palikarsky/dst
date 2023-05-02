f2ef0da323e4828e65611cff67ca8fe1065c92ad
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-05-02 16:03:43 +0200

    Management aspect completed

--- ass2-aop/src/main/java/dst/ass2/aop/management/ManagementAspect.java
+++ ass2-aop/src/main/java/dst/ass2/aop/management/ManagementAspect.java
@@ -1,7 +1,46 @@
 package dst.ass2.aop.management;
 
+import dst.ass2.aop.IPluginExecutable;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Aspect
 public class ManagementAspect {
 
-    // TODO
+    @Around("execution(* dst.ass2.aop.IPluginExecutable.execute(..))")
+    public Object around(ProceedingJoinPoint point) {
+        IPluginExecutable advisedPlugin = (IPluginExecutable) point.getTarget();
+
+
+        var myAnnotation = ((MethodSignature) point.getSignature()).getMethod()
+                .getAnnotation(Timeout.class);
+
+        if (myAnnotation != null) {
+
+            var timeout = myAnnotation.value();
+            TimerTask task = new TimerTask() {
+                public void run() {
+                    advisedPlugin.interrupted();
+                }
+            };
+            Timer timer = new Timer("Timer");
+
+            timer.schedule(task, timeout);
+        }
+
+        Object result;
+        try {
+            result = point.proceed();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
 
 }
