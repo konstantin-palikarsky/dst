9de7e439f13b943a291414ef42f134864fd30a4a
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-05-02 10:42:14 +0200

    current state of aope

--- ass2-aop/src/main/java/dst/ass2/aop/PluginExecutorFactory.java
+++ ass2-aop/src/main/java/dst/ass2/aop/PluginExecutorFactory.java
@@ -1,9 +1,16 @@
 package dst.ass2.aop;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
 public class PluginExecutorFactory {
 
     public static IPluginExecutor createPluginExecutor() {
-         return new PluginExecutor();
+        try {
+            return new PluginExecutor(FileSystems.getDefault().newWatchService());
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't instantiate new Watch Service for plugin executor");
+        }
     }
 
 }
