c7cd0cf58be4c09ad80234796a8cf8e43f2c418a
Konstantin Palikarsky <constantine.palicarsky@gmail.com>
2023-06-15 10:54:17 +0200

    final retouches

--- ass2-aop/src/main/java/dst/ass2/aop/impl/threads/DirectoryMonitorThread.java
+++ ass2-aop/src/main/java/dst/ass2/aop/impl/threads/DirectoryMonitorThread.java
@@ -60,6 +60,7 @@ public class DirectoryMonitorThread extends Thread {
                 }
                 key.reset();
             }
+
         } catch (InterruptedException ignored) {
         } finally {
             LOG.info("Exiting monitor for {}", dir.getName());
@@ -67,17 +68,32 @@ public class DirectoryMonitorThread extends Thread {
         }
     }
 
+    private void handleInitialPlugins() {
+        var files = Objects.requireNonNull(dir.listFiles());
+        if (files.length == 0) {
+            return;
+        }
+
+        Arrays.stream(files).forEach(this::handleFile);
+    }
+
+    private File eventToFile(WatchEvent<?> event) {
+        var filePath = dir.toPath().resolve(((Path) event.context()));
+
+        return filePath.toAbsolutePath().toFile();
+    }
 
     private void handleFile(File file) {
-
         if (!file.exists() || !file.getName().endsWith(".jar") ||
                 executedPlugins.contains(hashFile(file))) {
             return;
         }
+
         var pluginsToExecute = getExecutablePlugins(file);
         if (pluginsToExecute.isEmpty()) {
             return;
         }
+
         pluginsToExecute.forEach(x -> pluginRunnerPool.execute(new PluginExecutorThread(x)));
         executedPlugins.add(hashFile(file));
     }
@@ -91,14 +107,15 @@ public class DirectoryMonitorThread extends Thread {
             while ((entry = zip.getNextEntry()) != null) {
 
                 if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-                    String className = formatClassName(entry.getName());
-                    IPluginExecutable plugin = createPlugin(file, className);
+                    var className = formatClassName(entry.getName());
+                    var plugin = createPlugin(file, className);
+
                     if (plugin != null) {
                         returnList.add(plugin);
                     }
                 }
-
             }
+
         } catch (IOException e) {
             throw new RuntimeException("Exception while parsing jar: " + file.getName(), e);
         }
@@ -112,28 +129,16 @@ public class DirectoryMonitorThread extends Thread {
 
     private IPluginExecutable createPlugin(File file, String className) {
         try (URLClassLoader cl = new URLClassLoader(new URL[]{file.toURI().toURL()})) {
-            Class<?> loadedClass = cl.loadClass(className);
+            var loadedClass = cl.loadClass(className);
+
             if (IPluginExecutable.class.isAssignableFrom(loadedClass)) {
                 return (IPluginExecutable) loadedClass.getDeclaredConstructor().newInstance();
             }
+
         } catch (Exception ignored) {
         }
+
         return null;
-    }
-
-    private File eventToFile(WatchEvent<?> event) {
-        var filePath = dir.toPath().resolve(((Path) event.context()));
-
-        return filePath.toAbsolutePath().toFile();
-    }
-
-    private void handleInitialPlugins() {
-        var files = Objects.requireNonNull(dir.listFiles());
-        if (files.length == 0) {
-            return;
-        }
-
-        Arrays.stream(files).forEach(this::handleFile);
     }
 
     private byte[] hashFile(File file) {
@@ -142,7 +147,7 @@ public class DirectoryMonitorThread extends Thread {
             byte[] data = Files.readAllBytes(file.toPath());
             return MessageDigest.getInstance("MD5").digest(data);
         } catch (IOException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Catastrophic failure while hashing file in the MonitorThread", e);
         }
 
     }
